ff8d53fd147fa9ef357895a72f904dc5f2bd106d
Andrew Champion <andrew.champion@gmail.com>
2017-11-24 17:25:34 -0500

    Use failure crate for error handling
    Error messages and structure are trivial and untested. This also
    converts several non-impossible panics to errors.
    
    Resolves all warnings and clippy (although this is not yet enforced by
    CI).

--- schemer/src/lib.rs
+++ schemer/src/lib.rs
@@ -1,12 +1,16 @@
 extern crate daggy;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
 extern crate uuid;
 
 
 use std::collections::{HashMap, HashSet, VecDeque};
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 use daggy::Dag;
 use daggy::petgraph::EdgeDirection;
+use failure::Fail;
 use uuid::Uuid;
 
 
@@ -18,16 +22,66 @@ pub trait Migration {
     fn description(&self) -> &'static str;
 }
 
+#[derive(Debug)]
+pub enum MigrationDirection {
+    Up,
+    Down,
+}
+
+impl Display for MigrationDirection {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let printable = match *self {
+            MigrationDirection::Up => "up",
+            MigrationDirection::Down => "Down",
+        };
+        write!(f, "{}", printable)
+    }
+}
+
 pub trait Adapter {
     type MigrationType: Migration + ?Sized;
 
-    type Error: Debug; // TODO: How does this work with error_chain?
+    type Error: Debug + Fail;
 
     fn applied_migrations(&self) -> Result<HashSet<Uuid>, Self::Error>;
 
     fn apply_migration(&mut self, &Self::MigrationType) -> Result<(), Self::Error>;
 
     fn revert_migration(&mut self, &Self::MigrationType) -> Result<(), Self::Error>;
+}
+
+#[derive(Debug, Fail)]
+pub enum DependencyError {
+    #[fail(display = "Duplicate migration ID {}", _0)]
+    DuplicateId(Uuid),
+    #[fail(display = "Unknown migration ID {}", _0)]
+    UnknownId(Uuid),
+    #[fail(display = "Cyclic dependency cased by edge from migration IDs {} to {}", from, to)]
+    Cycle {
+        from: Uuid,
+        to: Uuid,
+    }
+}
+
+#[derive(Debug, Fail)]
+pub enum MigratorError<T: Debug + Fail> {
+    #[fail(display = "An error occurred due to migration dependencies")]
+    Dependency(#[cause] DependencyError),
+    #[fail(display = "An error occurred while interacting with the adapter.")]
+    Adapter(#[cause] T),
+    #[fail(display = "An error occurred while applying migration {} ({}) {}: {}.", id, description, direction, error)]
+    Migration {
+        id: Uuid,
+        description: &'static str,
+        direction: MigrationDirection,
+        #[cause] error: T,
+    },
+}
+
+impl<T: Debug + Fail> From<T> for MigratorError<T> {
+    fn from(error: T) -> MigratorError<T> {
+        MigratorError::Adapter(error)
+    }
 }
 
 pub struct Migrator<T: Adapter> {
@@ -45,15 +99,19 @@ impl<T: Adapter> Migrator<T> {
         }
     }
 
-    pub fn register(&mut self, migration: Box<T::MigrationType>) -> Result<(), T::Error> {
-        // TODO: check that this Id doesn't already exist in the graph.
+    pub fn register(&mut self, migration: Box<T::MigrationType>) -> Result<(), MigratorError<T::Error>> {
         let id = migration.id();
+        if self.id_map.contains_key(&id) {
+            return Err(MigratorError::Dependency(DependencyError::DuplicateId(id)))
+        }
         let depends = migration.dependencies();
         let migration_idx = self.dependencies.add_node(migration);
 
         for d in depends {
-            let parent_idx = self.id_map.get(&d).expect("TODO");
-            self.dependencies.add_edge(*parent_idx, migration_idx, ());
+            let parent_idx = self.id_map.get(&d)
+                .ok_or_else(|| MigratorError::Dependency(DependencyError::UnknownId(d)))?;
+            self.dependencies.add_edge(*parent_idx, migration_idx, ())
+                .or_else(|_| Err(MigratorError::Dependency(DependencyError::Cycle {from: d, to: id})))?;
         }
 
         self.id_map.insert(id, migration_idx);
@@ -66,10 +124,13 @@ impl<T: Adapter> Migrator<T> {
     /// (dependencies); if `Outgoing`, this is all descendents (dependents).
     /// If `id` is `None`, this is all migrations starting from the sources or
     /// the sinks, respectively.
-    fn induced_stream(&self, id: Option<Uuid>, dir: EdgeDirection) -> HashSet<Uuid> {
+    fn induced_stream(&self, id: Option<Uuid>, dir: EdgeDirection) -> Result<HashSet<Uuid>, DependencyError> {
         let mut target_ids = HashSet::new();
         match id {
             Some(id) => {
+                if !self.id_map.contains_key(&id) {
+                    return Err(DependencyError::UnknownId(id))
+                }
                 target_ids.insert(id);
             }
             // This will eventually yield all migrations, so could be optimized.
@@ -79,7 +140,9 @@ impl<T: Adapter> Migrator<T> {
                         .graph()
                         .externals(dir.opposite())
                         .map(|idx| {
-                            self.dependencies.node_weight(idx).expect("Impossible").id()
+                            self.dependencies.node_weight(idx)
+                                             .expect("Impossible: indices from this graph")
+                                             .id()
                         }),
                 )
             }
@@ -91,8 +154,10 @@ impl<T: Adapter> Migrator<T> {
             .map(|id| *self.id_map.get(id).expect("ID map is malformed"))
             .collect();
         while !to_visit.is_empty() {
-            let idx = to_visit.pop_front().expect("Not empty");
-            let id = self.dependencies.node_weight(idx).expect("Impossible").id();
+            let idx = to_visit.pop_front().expect("Impossible: not empty");
+            let id = self.dependencies.node_weight(idx)
+                                      .expect("Impossible: indices from this graph")
+                                      .id();
             target_ids.insert(id);
             to_visit.extend(
                 self.dependencies
@@ -101,49 +166,65 @@ impl<T: Adapter> Migrator<T> {
             );
         }
 
-        target_ids
+        Ok(target_ids)
     }
 
-    pub fn up(&mut self, to: Option<Uuid>) -> Result<(), T::Error> {
-        let target_ids = self.induced_stream(to, EdgeDirection::Incoming);
+    pub fn up(&mut self, to: Option<Uuid>) -> Result<(), MigratorError<T::Error>> {
+        let target_ids = self.induced_stream(to, EdgeDirection::Incoming)
+                             .map_err(MigratorError::Dependency)?;
 
         // TODO: This is assuming the applied_migrations state is consistent
         // with the dependency graph.
         let applied_migrations = self.adapter.applied_migrations()?;
         for idx in &daggy::petgraph::algo::toposort(self.dependencies.graph(), None)
-            .expect("Impossible because dependencies are a DAG")
+            .expect("Impossible: dependencies are a DAG")
         {
-            let migration = self.dependencies.node_weight(*idx).expect("Impossible");
+            let migration = self.dependencies.node_weight(*idx)
+                                             .expect("Impossible: indices from this graph");
             let id = migration.id();
             if applied_migrations.contains(&id) || !target_ids.contains(&id) {
                 continue;
             }
 
-            self.adapter.apply_migration(migration)?;
+            self.adapter.apply_migration(migration)
+                        .map_err(|e| MigratorError::Migration {
+                            id: id,
+                            description: migration.description(),
+                            direction: MigrationDirection::Up,
+                            error: e
+                        })?;
         }
 
         Ok(())
     }
 
-    pub fn down(&mut self, to: Option<Uuid>) -> Result<(), T::Error> {
-        let mut target_ids = self.induced_stream(to, EdgeDirection::Outgoing);
+    pub fn down(&mut self, to: Option<Uuid>) -> Result<(), MigratorError<T::Error>> {
+        let mut target_ids = self.induced_stream(to, EdgeDirection::Outgoing)
+                                 .map_err(MigratorError::Dependency)?;
         if let Some(sink_id) = to {
             target_ids.remove(&sink_id);
         }
 
         let applied_migrations = self.adapter.applied_migrations()?;
         for idx in daggy::petgraph::algo::toposort(self.dependencies.graph(), None)
-            .expect("Impossible because dependencies are a DAG")
+            .expect("Impossible: dependencies are a DAG")
             .iter()
             .rev()
         {
-            let migration = self.dependencies.node_weight(*idx).expect("Impossible");
+            let migration = self.dependencies.node_weight(*idx)
+                                             .expect("Impossible: indices from this graph");
             let id = migration.id();
             if !applied_migrations.contains(&id) || !target_ids.contains(&id) {
                 continue;
             }
 
-            self.adapter.revert_migration(migration)?;
+            self.adapter.revert_migration(migration)
+                        .map_err(|e| MigratorError::Migration {
+                            id: id,
+                            description: migration.description(),
+                            direction: MigrationDirection::Down,
+                            error: e
+                        })?;
         }
 
         Ok(())
@@ -282,7 +363,8 @@ pub mod tests {
         }
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Fail)]
+    #[fail(display = "An error occurred.")]
     struct DefaultTestAdapterError;
 
     impl Adapter for DefaultTestAdapter {
