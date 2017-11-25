46b80e72932ea9ec1b64a32444ae27198d5de3fc
Andrew Champion <andrew.champion@gmail.com>
2017-11-25 15:28:51 -0500

    Tests: add trivial branching DAG test

--- schemer/src/testing.rs
+++ schemer/src/testing.rs
@@ -65,7 +65,9 @@ macro_rules! test_schemer_adapter {
         test_schemer_adapter!($setup, $constructor,
             test_single_migration,
             test_migration_chain,
-            test_multi_component_dag,);
+            test_multi_component_dag,
+            test_branching_dag,
+        );
     };
     ($setup:stmt, $constructor:expr, $($test_fn:ident),* $(,)*) => {
         $(
@@ -227,5 +229,65 @@ pub fn test_multi_component_dag<A: TestAdapter>(adapter: A) {
         assert!(!applied.contains(&uuid2));
         assert!(!applied.contains(&uuid3));
         assert!(!applied.contains(&uuid4));
+    }
+}
+
+/// Test application and reversion on a branching DAG.
+pub fn test_branching_dag<A: TestAdapter>(adapter: A) {
+    let migration1 = A::mock(
+        Uuid::parse_str("bc960dc8-0e4a-4182-a62a-8e776d1e2b30").unwrap(),
+        HashSet::new(),
+    );
+    let migration2 = A::mock(
+        Uuid::parse_str("4885e8ab-dafa-4d76-a565-2dee8b04ef60").unwrap(),
+        HashSet::new(),
+    );
+    let migration3 = A::mock(
+        Uuid::parse_str("c5d07448-851f-45e8-8fa7-4823d5250609").unwrap(),
+        vec![migration1.id(), migration2.id()].into_iter().collect(),
+    );
+    let migration4 = A::mock(
+        Uuid::parse_str("9433a432-386f-467e-a59f-a9fb7e249767").unwrap(),
+        vec![migration3.id()].into_iter().collect(),
+    );
+    let migration5 = A::mock(
+        Uuid::parse_str("0940acb1-0e2e-4b99-9d69-2302a9c74524").unwrap(),
+        vec![migration3.id()].into_iter().collect(),
+    );
+
+    let uuid1 = migration1.id();
+    let uuid2 = migration2.id();
+    let uuid3 = migration3.id();
+    let uuid4 = migration4.id();
+    let uuid5 = migration5.id();
+
+    let mut migrator = Migrator::new(adapter);
+
+    migrator.register(migration1).expect("Migration 1 registration failed");
+    migrator.register(migration2).expect("Migration 2 registration failed");
+    migrator.register(migration3).expect("Migration 3 registration failed");
+    migrator.register(migration4).expect("Migration 4 registration failed");
+    migrator.register(migration5).expect("Migration 5 registration failed");
+
+    migrator.up(Some(uuid4)).expect("Up migration failed");
+
+    {
+        let applied = migrator.adapter.applied_migrations().unwrap();
+        assert!(applied.contains(&uuid1));
+        assert!(applied.contains(&uuid2));
+        assert!(applied.contains(&uuid3));
+        assert!(applied.contains(&uuid4));
+        assert!(!applied.contains(&uuid5));
+    }
+
+    migrator.down(Some(uuid1)).expect("Down migration failed");
+
+    {
+        let applied = migrator.adapter.applied_migrations().unwrap();
+        assert!(applied.contains(&uuid1));
+        assert!(applied.contains(&uuid2));
+        assert!(!applied.contains(&uuid3));
+        assert!(!applied.contains(&uuid4));
+        assert!(!applied.contains(&uuid5));
     }
 }
